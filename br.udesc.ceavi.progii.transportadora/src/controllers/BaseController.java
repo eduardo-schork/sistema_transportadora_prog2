c9d206b78e67786bbcba3e19ad5c2d1f22398656
André Cristen <andrecristenibirama@gmail.com>
2019-04-16 19:00:21 -0300

    Cometários para entendimento de acoes

--- br.udesc.ceavi.progii.transportadora/src/controllers/BaseController.java
+++ br.udesc.ceavi.progii.transportadora/src/controllers/BaseController.java
@@ -7,19 +7,37 @@ package controllers;
 
 /**
  *
- * @author T-Gamer
+ * @author André Cristen
  */
 public abstract class BaseController {
 
+    /**
+     * Metodo utilizado para abrir o form de insercao do objeto
+     */
     public abstract boolean add();
     
+    /**
+     * Metodo utilizado para publicar um objeto, utilizado na acao gravar do form 
+     */
     public abstract boolean addPost();
 
+    /**
+     * Metodo utilizado para abrir o from de edicao do objeto 
+     */
     public abstract boolean edit();
     
+    /**
+     * Metodo utilizado para publicar o objeto, utilizado na acao salvar do form
+     */
     public abstract boolean editPost();
 
+    /**
+     * Metodo que inicia a exclusao do objeto
+     */
     public abstract boolean delete();
 
+    /**
+     * Metodo utilizado para abrir o form de visualizacao do objeto
+     */
     public abstract boolean view();
 }
