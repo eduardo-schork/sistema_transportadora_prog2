dabd741ecc8ba73bb45d7d7648570742a326b1d0
Lucas Gonçalves <lucas.levibr@gmail.com>
2019-04-16 16:56:46 -0300

    Menu

--- br.udesc.ceavi.progii.transportadora/src/br/udesc/ceavi/progii/transudesc/view/menus/Menu.java
+++ br.udesc.ceavi.progii.transportadora/src/br/udesc/ceavi/progii/transudesc/view/menus/Menu.java
@@ -13,61 +13,74 @@ public class Menu extends JFrame {
      
     public Menu() {
          
-        setTitle("Menu TransUdesc");
-        setSize(1024, 768);
+        setTitle("TransUdesc");
+        setSize(1920, 1080);
          
         // Cria uma barra de menu para o JFrame
         JMenuBar menuBar = new JMenuBar();
          
-        // Adiciona a barra de menu ao  frame
+//         Adiciona a barra de menu ao  frame
         setJMenuBar(menuBar);
          
-        JMenu cadastros = new JMenu("Cadastros");
-        JMenu consulta = new JMenu("Consultas");
-        JMenu veiculos = new JMenu("Veículos");
-//        JMenu editMenu = new JMenu("Editar");
-        menuBar.add(cadastros);
-        menuBar.add(consulta);
-//        menuBar.add(editMenu);
+        JMenu crud_menu = new JMenu("CRUD");
+        JMenu relatorios_menu = new JMenu("Relatórios");
+        JMenu consulta_adm_menu = new JMenu("Consultas - Administrador");
+        JMenu verif_veiculos_menu = new JMenu("Verificar veículos");
+        JMenu cargas_menu = new JMenu("Cargas");
+        JMenu consulta_entrega = new JMenu("Consultar Entregas");
+        
+//      Coloca na barra de menu
+        menuBar.add(crud_menu);
+        menuBar.add(consulta_adm_menu);
+        menuBar.add(verif_veiculos_menu);
+        menuBar.add(cargas_menu);
+        menuBar.add(relatorios_menu);
+        menuBar.add(consulta_entrega);
          
-        // Cria e adiciona um item simples para o menu
+        // Cria e adiciona um item para o menu
         JMenuItem carregamento = new JMenuItem("Carregamento");
-        JMenuItem entrega = new JMenuItem("Entrega");
+        JMenuItem entrega_crud = new JMenuItem("Entrega");
         JMenuItem filial = new JMenuItem("Filial");
         JMenuItem pessoa_fisica = new JMenuItem("Pessoa Física");
         JMenuItem pessoa_juridica = new JMenuItem("Pessoa Juridica");
-        JMenuItem situacao_entrega = new JMenuItem("Situação entrega");
-        JMenuItem status_veiculo = new JMenuItem("Status Veiculo");
         JMenuItem usuario = new JMenuItem("Usuario");
         JMenuItem veiculo = new JMenuItem("Veiculo");
         JMenuItem volume = new JMenuItem("Volume");
-        
         JMenuItem cargas = new JMenuItem("Cargas");
         JMenuItem clientes = new JMenuItem("Clientes");
         JMenuItem consulta_veiculos = new JMenuItem("Veiculos");
         JMenuItem motoristas = new JMenuItem("Motoristas");
         JMenuItem entregas = new JMenuItem("Entregas");
         JMenuItem volumes = new JMenuItem("Volumes");
+        JMenuItem entrega_consult = new JMenuItem("Consultar entrega");
+        JMenuItem transportadora = new JMenuItem("Trasportadora Redespacho");
+        
+        JMenuItem veiculos_verif_em_reparo = new JMenuItem("Veículos em reparo");
+        JMenuItem veiculos_verif_aguard_reparo = new JMenuItem("Veículos aguardando reparo");
+        
         
         ButtonGroup bg = new ButtonGroup();
-        cadastros.add(carregamento);
-        cadastros.add(entrega);
-//        cadastros.addSeparator();
-        cadastros.add(filial);
-        cadastros.add(pessoa_fisica);
-        cadastros.add(pessoa_juridica);
-        cadastros.add(situacao_entrega);
-        cadastros.add(status_veiculo);
-        cadastros.add(usuario);
-        cadastros.add(veiculo);
-        cadastros.add(volume);
+        crud_menu.add(carregamento);
+        crud_menu.add(entrega_crud);
+        crud_menu.add(filial);
+        crud_menu.add(pessoa_fisica);
+        crud_menu.add(pessoa_juridica);
+        crud_menu.add(usuario);
+        crud_menu.add(veiculo);
+        crud_menu.add(volume);
+        crud_menu.add(transportadora);
         
-        consulta.add(cargas);
-        consulta.add(clientes);
-        consulta.add(consulta_veiculos);
-        consulta.add(motoristas);
-        consulta.add(entregas);
-        consulta.add(volumes);
+        verif_veiculos_menu.add(veiculos_verif_em_reparo);
+        verif_veiculos_menu.add(veiculos_verif_aguard_reparo);
+        
+        consulta_adm_menu.add(cargas);
+        consulta_adm_menu.add(clientes);
+        consulta_adm_menu.add(consulta_veiculos);
+        consulta_adm_menu.add(motoristas);
+        consulta_adm_menu.add(entregas);
+        consulta_adm_menu.add(volumes);
+        
+        consulta_entrega.add(entrega_consult);
     }
 }
     
